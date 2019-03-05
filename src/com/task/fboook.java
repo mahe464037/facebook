00b425644148c24566aceb07586a28d9111cbc55
unknown <464037@gmail.com>
2019-03-05 15:58:58 +0530

    First commit

---
+++ src/com/task/fboook.java
@@ -0,0 +1,37 @@
+package com.task;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+
+public class fboook {
+	
+	public WebDriver driver;
+  @Parameters({"browser"})
+  @BeforeTest
+  public void beforeTest(String browser) {
+	  if(browser.equalsIgnoreCase("chrome"))
+	  {
+		  System.out.println("In the open brwoser");
+		  System.setProperty("webdriver.chrome.driver", "C:\\Users\\mahendra\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		  driver= new ChromeDriver();
+		  System.out.println("Browser Open Successfully");
+	  }
+  }
+  @Parameters({"url"})
+  @Test(priority=1)
+  public void enterurl(String urlname)
+  {
+	  driver.get(urlname);
+	  System.out.println("Url entered successfully");
+	  driver.manage().window().maximize();
+  }
+@Test
+public void f()
+{
+	
+}
+}
